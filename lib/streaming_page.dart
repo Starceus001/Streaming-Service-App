db9421d34fd218d2135a9e1983ce1f89a5f8b95d
D3SM0S <brentjn89@gmail.com>
2023-11-21 16:15:15 +0100

    verdere ontwikkeling aan het streamen naar twitch

--- lib/streaming_page.dart
+++ lib/streaming_page.dart
@@ -1,4 +1,7 @@
+// stream key: live_993744240_DufYHsGijuzj1bYQ4TFNR0aK3eHfXN
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import 'package:camera/camera.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,17 +16,16 @@ class _StreamingPageState extends State<StreamingPage> {
   late CameraController _controller;
   TextEditingController _streamKeyController = TextEditingController();
 
-  // Twitch API-gegevens
   String clientId = '1k07v26rojuhagzfrahvzqr8d37989';
-  String clientSecret = 'a5bdn6uo9pq2rwfzm9rk6jnc3kf8vg';
-  String twitchUrl = 'http://localhost:0001/oauth';
+  String redirectUri = 'http://localhost:0001/oauth';
+  String scope = 'channel:manage:streamkey';
+
+  late WebViewController _webViewController; // Hier toegevoegd
 
   @override
   void initState() {
     super.initState();
-    // Get a list of available cameras.
     availableCameras().then((cameras) {
-      // Initialize the camera controller
       _controller = CameraController(cameras[0], ResolutionPreset.medium);
       _controller.initialize().then((_) {
         if (!mounted) {
@@ -41,66 +43,43 @@ class _StreamingPageState extends State<StreamingPage> {
     super.dispose();
   }
 
-  Future<String> getTwitchAccessToken(String clientId, String clientSecret) async {
-    try {
-      final response = await http.post(
-        Uri.parse('$twitchUrl/token'), // Twitch OAuth URL
-        body: {
-          'client_id': clientId,
-          'client_secret': clientSecret,
-          'grant_type': 'client_credentials',
-        },
-      );
-
-      if (response.statusCode == 200) {
-        // Parse the JSON response and return the access token
-        return response.body;
-      } else {
-        // Handle errors
-        print('Error getting Twitch access token: ${response.statusCode}');
-        return '';
-      }
-    } catch (e) {
-      // Handle exceptions
-      print('Exception getting Twitch access token: $e');
-      return '';
-    }
-  }
-
-  Future<void> startTwitchStream(String clientId, String accessToken, String streamKey) async {
-    try {
-      final response = await http.post(
-        Uri.parse('https://api.twitch.tv/helix/streams'),
-        headers: {
-          'Client-ID': clientId,
-          'Authorization': 'Bearer $accessToken',
-          'Content-Type': 'application/json',
-        },
-        body: '{"title": "My Twitch Stream", "game_id": "game_id_here"}',
-      );
-
-      if (response.statusCode == 200) {
-        // Stream started successfully
-        print('Stream started successfully');
-      } else {
-        // Handle errors
-        print('Error starting stream: ${response.statusCode}');
-      }
-    } catch (e) {
-      // Handle exceptions
-      print('Exception starting stream: $e');
-    }
-  }
-
-  void _startStreaming() async {
-    // Get the stream key from the text field
+  Future<void> _startStreaming(String accessToken) async {
     String streamKey = _streamKeyController.text;
 
-    // Get the Twitch access token
-    String accessToken = await getTwitchAccessToken(clientId, clientSecret);
+    if (_controller.value.isInitialized) {
+      try {
+        final response = await http.post(
+          Uri.parse('https://api.twitch.tv/helix/streams'),
+          headers: {
+            'Client-ID': clientId,
+            'Authorization': 'Bearer $accessToken',
+            'Content-Type': 'application/json',
+          },
+          body: '{"title": "My Twitch Stream", "Test, Test": "Test, Test"}',
+        );
 
-    // Start the Twitch stream
-    await startTwitchStream(clientId, accessToken, streamKey);
+        if (response.statusCode == 200) {
+          print('Stream started successfully');
+        } else {
+          print('Error starting stream: ${response.statusCode}');
+        }
+      } catch (e) {
+        print('Exception starting stream: $e');
+      }
+    } else {
+      print('Camera is not initialized.');
+    }
+  }
+
+  void _openTwitchAuthPage() {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => TwitchLoginScreen(
+        clientId: clientId,
+        redirectUri: redirectUri,
+        scope: scope,
+        onAccessTokenReceived: _startStreaming,
+      ),
+    ));
   }
 
   @override
@@ -130,7 +109,7 @@ class _StreamingPageState extends State<StreamingPage> {
                   ),
                   SizedBox(height: 16),
                   ElevatedButton(
-                    onPressed: _startStreaming,
+                    onPressed: _openTwitchAuthPage,
                     child: Text('Start Streaming'),
                   ),
                 ],
@@ -138,6 +117,48 @@ class _StreamingPageState extends State<StreamingPage> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TwitchLoginScreen extends StatelessWidget {
+  final String clientId;
+  final String redirectUri;
+  final String scope;
+  final Function(String) onAccessTokenReceived;
+
+  TwitchLoginScreen({
+    required this.clientId,
+    required this.redirectUri,
+    required this.scope,
+    required this.onAccessTokenReceived,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Twitch Login'),
+      ),
+      body: WebView(
+        initialUrl:
+            'https://id.twitch.tv/oauth2/authorize?client_id=$clientId&redirect_uri=$redirectUri&response_type=token&scope=$scope',
+        javascriptMode: JavascriptMode.unrestricted,
+        navigationDelegate: (NavigationRequest request) {
+          if (request.url.startsWith(redirectUri)) {
+            Uri uri = Uri.parse(request.url);
+            String accessToken = uri.fragment.substring(uri.fragment.indexOf('access_token=') + 13);
+            accessToken = accessToken.substring(0, accessToken.indexOf('&'));
+
+            onAccessTokenReceived(accessToken);
+
+            Navigator.pop(context);
+
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
       ),
     );
   }

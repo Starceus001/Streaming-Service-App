530164405b9dee9e57e1f92dc99acfafb5bc05b5
Starceus001 <guidoannema@hotmail.com>
2023-11-21 14:44:11 +0100

    Tested: Added cropping after picking an image from the gallery with cropped image preview and confirmation before final cropping and saving. Awaiting Brent code before sending cropped image to webserver.

--- lib/upload_page.dart
+++ lib/upload_page.dart
@@ -1,43 +1,132 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as img;
+import 'dart:typed_data';
 
-import 'package:image_cropper/image_cropper.dart';
-
-Future<void> _pickImage() async {
-  final imagePicker = ImagePicker();
-  final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
-
-  if (pickedFile != null) {
-    File pickedImageFile = File(pickedFile.path);
-
-    // crop the image with a set diameter
-    File croppedFile = await ImageCropper().cropImage(
-      sourcePath: pickedImageFile.path,
-      maxWidth: 224, // Set your desired diameter here
-      maxHeight: 224, // Set your desired diameter here
-      aspectRatio: const CropAspectRatio(ratioX: 1, ratioY: 1), // Crop to a square
-      compressQuality: 100, // 100 means no compression
-      androidUiSettings: const AndroidUiSettings(
-        toolbarTitle: 'Crop Image',
-        toolbarColor: Colors.blue,
-        toolbarWidgetColor: Colors.white,
-        initAspectRatio: CropAspectRatioPreset.original,
-        lockAspectRatio: true,
-      ),
-    );
-    // display the picked (and cropped) image
-    // Image.file(pickedImageFile, width: 200, height: 200);    // select the full image
-    Image.file(croppedFile!, width: 200, height: 200);           // select the cropped image
-    }
-}
-
-class UploadPage extends StatelessWidget {
+class UploadPage extends StatefulWidget {
   const UploadPage({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
+  _UploadPageState createState() => _UploadPageState();
+}
+
+class _UploadPageState extends State<UploadPage> {
+  File? pickedImageFile;
+  img.Image? croppedImage;
+  img.Image? tempCroppedImage;
+
+  // pick the image from the gallery
+  Future<void> _pickImage() async {
+    final imagePicker = ImagePicker();
+    try {
+      final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
+
+      if (pickedFile != null) {
+        setState(() {
+          pickedImageFile = File(pickedFile.path);
+        });
+
+        // call the cropping function
+        _cropImage();
+      }
+    } catch (e) {
+      Text("Error picking image: $e");
+    }
+  }
+
+// crop the image to show and ask for confirmation
+void _cropImage() {
+  if (pickedImageFile != null) {
+    // load the picked image
+    final rawImage = img.decodeImage(File(pickedImageFile!.path).readAsBytesSync())!;
+
+    // set targetSize (when done, get size from settings page)
+    const double targetSize = 224.0;
+
+    // crop the image to a circle
+    tempCroppedImage = img.copyCropCircle(rawImage, radius: targetSize.toInt());
+
+    // show the confirmation dialog
+    _showConfirmationDialog();
+  }
+}
+
+// give a popup where the user can accept or deny the selected (cropped) image
+void _showConfirmationDialog() {
+  // set targetSize (when done, get size from settings page)
+    const double targetSize = 224.0;
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      final currentContext = context;
+
+      return AlertDialog(
+        title: const Text('Confirm Image'),
+        content: Column(
+          children: [
+            if (tempCroppedImage != null)
+              Image.memory(
+                Uint8List.fromList(img.encodePng(tempCroppedImage!)),
+                width: targetSize,
+                height: targetSize,
+              ),
+            const Text('The selected image will be displayed as shown above, would you like to continue?'),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(currentContext).pop();
+            },
+            child: const Text('No'),
+          ),
+          TextButton(
+            onPressed: () {
+              _confirmCrop();
+              Navigator.of(currentContext).pop();
+            },
+            child: const Text('Yes'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+// the user confirms the cropped image, send it to the webserver
+void _confirmCrop() {
+  if (pickedImageFile != null && tempCroppedImage != null) {
+
+    // if user confirms, save the cropped image to the main variable
+    croppedImage = tempCroppedImage;
+
+    // save the cropped image back to the file
+    File(pickedImageFile!.path).writeAsBytesSync(img.encodePng(croppedImage!));
+
+    // trigger a rebuild of the widget tree
+    setState(() {});
+
+    // Perform further actions with the cropped file (croppedImage now holds the final cropped image!)
+// "Brent Code" Add brent his code here to send croppedImage to the selected webserver, give a problem message if no server has been selected in settings
+    // For example, save the cropped file path or upload it to a server
+    // finalVariable = pickedImageFile;
+    // performFurtherAction();
+    // You can show a confirmation message or navigate to the next screen here
+    // print("Cropped image confirmed and stored: ${pickedImageFile!.path}");
+  } else {
+    // handle the case where cropping or saving failed
+    const Text("Saving cropped image failed");
+  }
+}
+
+  @override
   Widget build(BuildContext context) {
-    //code can be added from here
+    // set targetSize (when done, get size from settings page)
+    const double targetSize = 224.0;
+
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -46,19 +135,31 @@ class UploadPage extends StatelessWidget {
             'Upload Page',
             style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
           ),
+          // button to pick and crop an image
           ElevatedButton(
             onPressed: () {
-              // Call the _pickImage function when the button is pressed
+              // call the _pickImage function when the button is pressed
               _pickImage();
             },
             style: ElevatedButton.styleFrom(
-              backgroundColor : Colors.blue, // Change background color to blue
-              foregroundColor : Colors.white, // Change text color to white
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
             ),
-            child: const Text('Pick Image'),
+            child: const Text('Select Image'),
           ),
+          // display the final cropped image in the app
+          if (croppedImage != null)
+            Column(
+              children: [
+                Image.memory(
+                  Uint8List.fromList(img.encodePng(croppedImage!)),
+                  width: targetSize,
+                  height: targetSize,
+                ),
+              ],
+            ),
         ],
-      )
+      ),
     );
   }
 }

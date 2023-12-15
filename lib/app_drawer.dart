0de7c14c2e387516ea260a8a882ecc9308a4d1d0
Starceus001 <guidoannema@hotmail.com>
2023-12-15 15:50:57 +0100

    Changed menu colors and style

--- lib/app_drawer.dart
+++ lib/app_drawer.dart
@@ -13,20 +13,51 @@ class AppDrawer extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: [
           DrawerHeader(
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 139, 72, 255),
+            decoration: const BoxDecoration(
+              color: Color(0xFF192442),
             ),
-            child: Text(
-              'Navigator',
-              style: TextStyle(
-                fontFamily: 'Exo',
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
-              ),
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 0.0,
+                  left: 0.0,
+                  child: Container(
+                    width: 100.0,
+                    height: 100.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.black.withOpacity(0.2), width: 2.0),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: -25.0,
+                  left: -25.0,
+                  child: Container(
+                    width: 150.0,
+                    height: 150.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.black.withOpacity(0.2), width: 2.0),
+                    ),
+                  ),
+                ),
+                const Center(
+                  child: Text(
+                    'Navigator',
+                    style: TextStyle(
+                      fontFamily: 'Exo',
+                      fontSize: 30.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           ListTile(
-            title: Text('Home'),
+            title: const Text('Home'),
             selected: selectedIndex == 0,
             onTap: () {
               onItemTapped(0);
@@ -34,7 +65,7 @@ class AppDrawer extends StatelessWidget {
             },
           ),
           ListTile(
-            title: Text('Upload'),
+            title: const Text('Upload'),
             selected: selectedIndex == 1,
             onTap: () {
               onItemTapped(1);
@@ -42,7 +73,7 @@ class AppDrawer extends StatelessWidget {
             },
           ),
           ListTile(
-            title: Text('Streaming'),
+            title: const Text('Streaming'),
             selected: selectedIndex == 2,
             onTap: () {
               onItemTapped(2);
@@ -50,7 +81,7 @@ class AppDrawer extends StatelessWidget {
             },
           ),
           ListTile(
-            title: Text('Settings'),
+            title: const Text('Settings'),
             selected: selectedIndex == 3,
             onTap: () {
               onItemTapped(3);
